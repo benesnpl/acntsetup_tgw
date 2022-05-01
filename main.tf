5da4913f67bedeca73ee1ae4da19b283479fbb8b
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-01 09:56:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -122,8 +122,8 @@ resource "aws_ec2_transit_gateway" "main_tgw" {
   auto_accept_shared_attachments = "enable"
 }
 resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
-  count = length(var.subnets_cidr_private)
-  subnet_ids         = [
+  
+  subnet_ids         = [aws_subnet.private.id]
   transit_gateway_id = aws_ec2_transit_gateway.main_tgw.id
   vpc_id             = aws_vpc.main_vpc.id
   appliance_mode_support = "enable"
