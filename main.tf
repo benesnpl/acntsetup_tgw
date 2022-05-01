ee7347b9f5507548a7ba7cda8385da67bac10de9
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-01 12:28:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_ec2_transit_gateway" "main_tgw" {
 data "aws_subnet_ids" "private" {
   filter {
     name   = "tag:Name"
-    values = ["Subnet-Private*] # insert values here
+    values = ["Subnet-Private*"] # insert values here
   }
 }
   
